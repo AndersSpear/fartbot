1c2d493df0757d81afba4e15fa5aad67a455ff92
Anders Spear <anders@aspear.net>
2023-02-04 17:32:08 -0500

    WHYISNTTHISWORKING

--- fartbot.py
+++ fartbot.py
@@ -29,7 +29,8 @@ class MyClient(discord.Client):
                         row = await cursor.fetchone()
                         today = date.today()
                         if(row == None):
-                            await db.execute(f"INSERT INTO fartstreak (userid, longeststreak_start_date, longeststreak_end_date, longeststreak_length, currentstreak_start_date, currentstreak_end_date, currentstreak_length, pfp, name) VALUES ({message.author.id}, '{today}', '{today}', 1, '{today}', '{today}', 1, '{message.author.avatar.url}', '{message.author.name}');")
+                            
+                            await db.execute(f"INSERT INTO fartstreak (userid, longeststreak_start_date, longeststreak_end_date, longeststreak_length, currentstreak_start_date, currentstreak_end_date, currentstreak_length, pfp, name) VALUES ({message.author.id}, '{today}', '{today}', 1, '{today}', '{today}', 1, '{message.author.display_avatar.url}', '{message.author.name}');")
                             await db.commit()
                         else:
                             #print(f'row: {row}')
@@ -75,7 +76,7 @@ intents.members = True
 
 client = MyClient(intents=intents)
 #tree = app_commands.CommandTree(client)
-
+#testinggit
 # @tree.command(name = "update", description = "adds pfp links and names to db", guild=discord.Object(id=1047644766311043162)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
 # async def first_command(interaction):
 #     async with aiosqlite.connect("/home/pi/projects/fartbot/fartstreak.db") as db:
