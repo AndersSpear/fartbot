60890014ab6b2cb3712058b482c7b351e2af2ccc
aspear <aspr@umd.edu>
2023-02-22 01:29:32 -0500

    added https

--- main.py
+++ main.py
@@ -16,5 +16,5 @@ def main():
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host='0.0.0.0', port=80)
+    serve(app, host='0.0.0.0', port=80, url_scheme='https')
     #app.run( host='0.0.0.0', port=80)
