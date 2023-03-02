d5a42c201f9bd5a3061d69f3dffd81193f5727fe
aspear <anders@aspear.net>
2023-03-01 22:49:46 -0500

    trying reset script

--- reset_current_streaks.py
+++ reset_current_streaks.py
@@ -13,23 +13,10 @@ for row in rows:
      print(row[4])
      print(str(today - timedelta(days = 1)))
      print(str(today - timedelta(days = 2)))
-
-
-
-# db.execute(f'SELECT * FROM fartstreak WHERE userid = {message.author.id};') as cursor:
-
-
-# async with aiosqlite.connect("/home/pi/projects/fartbot/fartstreak.db") as db:
-#         async with db.execute('SELECT * FROM fartstreak') as cursor:
-#             rows = await cursor.fetchall()
-#             for row in rows:
-#                 user = await client.fetch_user(row[0])
-#                 #print(user)
-#                 await db.execute(f"""UPDATE fartstreak 
-#                                     SET pfp = '{user.display_avatar.url}',
-#                                         name = '{user.name}',
-#                                         total = {max(row[9],row[5])}
-#                                     WHERE
-#                                         userid = {row[0]};""")
-#                 await db.commit()
-#     await interaction.followup.send(content='done', ephemeral = True)
+     if(row[4] != str(today) and row[4] != str(today - timedelta(days = 1))):
+         print('reset')
+         cur.execute(f"""UPDATE fartstreak 
+         SET currentstreak_length = 0
+         WHERE
+             userid = {row[0]};""")
+         cur.commit()
