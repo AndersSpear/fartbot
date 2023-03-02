9ae53e98644e7c90cb0fa1de0e14810510e94448
aspear <aspr@umd.edu>
2023-03-01 20:53:53 -0500

    changed site

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
     rend = sorted(res, key=lambda x: x[5])
     out = rend[::-1]
     for entry in out:
+        print(type(entry))
         entry.pop("currentstreak_start_date")
         entry.pop("currentstreak_end_date")
         entry.pop("longeststreak_start_date")
