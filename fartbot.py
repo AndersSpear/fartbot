5e5b0c708241389989e50b33fe90353d5f8f72b0
aspear <anders@aspear.net>
2023-04-18 23:20:18 -0400

    better logging lol

--- fartbot.py
+++ fartbot.py
@@ -32,7 +32,9 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         #print(f'Message from {message.author}: {message.content}')
         if(message.channel.id == 1047644766877270038):
+            print(message.author),
             print(message.content)
+            
             if(message.author.get_role(1097972642742550549) != None and message.content== "poo clan"):
                 emoji = client.get_emoji(1097984026297516034)
                 await message.add_reaction(emoji)
