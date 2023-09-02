876648d6a6a9ba7a8ff91b0c15e8518d2b93f6fa
ocpkn <113632923+ocpkn@users.noreply.github.com>
2023-09-02 14:02:49 -0400

    Create example_config.py

---
+++ example_config.py
@@ -0,0 +1,8 @@
+# Actual config file has auth token in it (scary!) so it is in .gitignore
+# Here is what the config.py should look like
+token = ''    # auth token
+guild = 0     # guild id
+channel = 0   # channel id where you can only say 'fart club'
+poo_clan = 0  # role id of poo clan
+general = 0   # role id to let people talk in general
+dbpath = ''   # /path/to/your/sqlite/database.db
